4aa69f6615fbeee937ba0049629c16aebba3c894
patewy <andrey.ignatov.07@mail.ru>
2026-04-21 12:26:09 +0300

    фикс 4

--- script.js
+++ script.js
@@ -687,15 +687,3 @@ document.addEventListener('click', (event) => {
     }
 });
 
-const fixSafariHeight = () => {
-    const sidebar = document.getElementById('sidebar');
-    if (sidebar) {
-        // Устанавливаем высоту равную внутренней высоте окна
-        sidebar.style.height = `${window.innerHeight}px`;
-    }
-};
-
-window.addEventListener('resize', fixSafariHeight);
-window.addEventListener('orientationchange', fixSafariHeight);
-// Вызываем сразу
-fixSafariHeight();
